ef79f4942824139a06791c94e1874971e374a8e0
Lucas Dias Nardino <lucas.diasnardino@gmail.com>
2021-08-23 16:16:22 -0300

    Code to find blendshapes' names. (Wll be used in future update)

---
+++ Assets/BlendShapes.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlendShapes : MonoBehaviour
+{
+    public GameObject Body;
+
+    public string[] getBlendShapeNames(GameObject Body)
+    {
+        SkinnedMeshRenderer head = Body.GetComponent<SkinnedMeshRenderer>();
+        Mesh m = head.sharedMesh;
+
+        string[] arr;
+        arr = new string[m.blendShapeCount];
+
+        for(int i =0; i< m.blendShapeCount; i++)
+        {
+            string s = m.GetBlendShapeName(i);
+            print("BlendShape: " + i + " " + s);
+
+            arr[i] = s;
+        }
+
+
+        return arr;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        getBlendShapeNames(Body);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
